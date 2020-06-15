362adcb241fa34b98840bb602ccc976cad31c721
SHER321 <66766275+SHER321@users.noreply.github.com>
2020-06-15 17:53:21 +0530

    CMDS.py

---
+++ Update CMDS.py
@@ -0,0 +1,70 @@
+from typing import Optional, List
+
+from telegram import Message, Update, Bot, User
+
+from telegram import MessageEntity
+
+from telegram.ext import Filters, MessageHandler, run_async
+
+from tg_bot import dispatcher, LOGGER
+
+from tg_bot.modules.disable import DisableAbleCommandHandler
+
+from telegraph import Telegraph, upload_file
+
+@run_async
+
+def media_telegraph(bot: Bot, update: Update):
+
+    msg = update.effective_message # type: Optional[Message]
+
+@run_async
+
+def post_telegraph(bot: Bot, update: Update, args: List[str]):
+
+    short_name = "Created By @NGYNY 😎"
+
+    msg = update.effective_message # type: Optional[Message]
+
+    telegraph = Telegraph()
+
+    r = telegraph.create_account(short_name=short_name)
+
+    auth_url = r["auth_url"]
+
+    LOGGER.info(auth_url)
+
+    title_of_page = " ".join(args)
+
+    page_content = msg.reply_to_message.text
+
+    page_content = page_content.replace("\n", "<br>")
+
+    response = telegraph.create_page(
+
+        title_of_page,
+
+        html_content=page_content
+
+    )
+
+    msg.reply_text("https://telegra.ph/{}".format(response["path"]))
+
+__help__ = """
+
+○ My Name : *Filter Bot*
+
+○ Creator : [N I K H I L](http://telegram.dog/NGYNY)
+
+○ Credits : `Everyone in this journey`
+
+○ Language : Python3
+
+○ Channel : [CLICK HERE](https://t.me/ALL_MOVIES_LIABRARY)
+
+○ Group : 👉 [Click Here](https://t.me/MOVIERG_NY)
+
+"""
+
+__mod_name__ = "⚠️About⚠️"
+
